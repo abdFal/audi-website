389c395280572e5e6fdb960b77413e9eaa9d7142
unknown <naufaldec07@gmail.com>
2022-12-14 09:37:39 +0700

    p

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ $(function () {
     return "";
   });
 });
+
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+  document.title = "Sini dong :( ";
+});
+window.addEventListener("focus", () => {
+  document.title = docTitle;
+});
